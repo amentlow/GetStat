d4646531bd6ac2268d9e321d82dd96608f3aeb0d
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2018-03-30 08:21:53 +0300

    bot code added

---
+++ main.rb
@@ -0,0 +1,18 @@
+
+require 'rubygems'
+require 'telegram/bot'
+
+token = '577447618:AAHJwCZ51XLxik596Howxf2yVjcpixXMBck'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
+    when '/end'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
+    else
+      bot.api.send_message(chat_id: message.chat.id, text: " Безусловно ")
+    end
+  end
+end
